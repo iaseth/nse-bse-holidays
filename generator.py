980f22f79c578b8d8ff754fd67da071f9b3a1919
Ankur Seth <iankurseth@gmail.com>
2025-10-30 18:46:36 +0530

    Added month and weekday

--- generator.py
+++ generator.py
@@ -12,10 +12,13 @@ class Holiday:
 
 		self.year = self.date.year
 		self.formatted = self.date.strftime("%d %B %Y")
+		self.date_str = str(self.date)
+		self.month_str = self.date.strftime("%B")
+		self.weekday_str = self.date.strftime("%A")
 
 	@property
 	def cols(self):
-		return [self.formatted, self.name]
+		return [self.month_str, self.date_str, self.weekday_str, self.name]
 
 	def __repr__(self):
 		return f"Holiday ({self.formatted})"
@@ -33,7 +36,7 @@ def main():
 
 		with open(out_file, "w") as f:
 			f.write(f"# Stock Market Holidays in {year}\n\n")
-			f.write(tabulate(holidays_in_year, tablefmt="github", headers=["Date", "Holiday"]))
+			f.write(tabulate(holidays_in_year, tablefmt="github", headers=["Month", "Date", "Day", "Holiday"]))
 			f.write("\n\n")
 		print(f"Saved: {out_file} ({len(holidays_in_year)} holidays)")
 
